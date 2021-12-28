a8269e931f6d19e824611682d88cd076bb64a91e
pelmenmensupper <93839869+pelmenmensupper@users.noreply.github.com>
2021-12-28 19:08:47 +0300

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+
+public class HelloWorld{
+
+
+    
+public static void main(String []args){
+ 
+System.out.println("Hello World");
+int a = 58;
+System.out.print(a);
+
+String [] c = new String [2];
+
+c[0] = "Hello";
+
+c[1] = "World";
+
+System.out.println(c[0] + " " + c[1]);
+
+Scanner input = new Scanner(System.in);
+
+String str = input.nextLine();
+
+System.out.println(str);
+
+}
+
+}
